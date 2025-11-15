c263b95be282a554ce32e9304bc517ad9353cc4c
Severin Leander <gasserseverin1@gmail.com>
2025-11-15 12:38:20 +0100

    Add service worker for caching and offline support

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = "fokus-kompass-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) =>
+          key !== CACHE_NAME ? caches.delete(key) : Promise.resolve()
+        )
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then(
+      (response) => response || fetch(event.request)
+    )
+  );
+});
